731f8b441039aa05c7b76144cc2fb42b0024aa6f
Dmitriy Bessarab <bessarabds@gmail.com>
2024-05-20 16:45:14 +0500

    sobel and treshold

--- SobelFilterTask.cs
+++ SobelFilterTask.cs
@@ -1,26 +1,42 @@
-﻿using System;
+﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
+using SkiaSharp;
+using System;
 
 namespace Recognizer;
 internal static class SobelFilterTask
 {
     public static double[,] SobelFilter(double[,] g, double[,] sx)
     {
+        var sSize = sx.GetLength(0);
         var width = g.GetLength(0);
         var height = g.GetLength(1);
         var result = new double[width, height];
-        for (int x = 1; x < width - 1; x++)
-            for (int y = 1; y < height - 1; y++)
+        for (int y = sSize / 2; y < height - sSize / 2; y++)
+            for (int x = sSize / 2; x < width - sSize / 2; x++)
             {
-                // Вместо этого кода должно быть поэлементное умножение матриц sx и полученной транспонированием из неё sy на окрестность точки (x, y)
-                // Такая операция ещё называется свёрткой (Сonvolution)
-                var gx = 
-                    -g[x - 1, y - 1] - 2 * g[x, y - 1] - g[x + 1, y - 1] 
-                    + g[x - 1, y + 1] + 2 * g[x, y + 1] + g[x + 1, y + 1];
-                var gy = 
-                    -g[x - 1, y - 1] - 2 * g[x - 1, y] - g[x - 1, y + 1] 
-                    + g[x + 1, y - 1] + 2 * g[x + 1, y] + g[x + 1, y + 1];
-                result[x, y] = Math.Sqrt(gx * gx + gy * gy);
+                result[x, y] = FilteredPoint(g, sx, x, y);
             }
         return result;
+    }
+
+    public static double FilteredPoint(double[,] g, double[,] sx, int pointX, int pointY)
+    {
+        var sSize = sx.GetLength(0);
+        var xG = pointX - sSize / 2;
+        var yG = pointY - sSize / 2;
+        double gx = 0;
+        double gy = 0;
+        for (var y = 0; y < sSize; y++)
+        {
+            xG = pointX - sSize / 2; ;
+            for (var x = 0; x < sSize; x++)
+            {
+                gx += g[xG, yG] * sx[x, y];
+                gy += g[xG, yG] * sx[y, x];
+                xG++;
+            }
+            yG++;
+        }
+        return Math.Sqrt(gx * gx + gy * gy);
     }
 }
--- ThresholdFilterTask.cs
+++ ThresholdFilterTask.cs
@@ -1,9 +1,47 @@
-﻿namespace Recognizer;
+﻿using Avalonia.Controls;
+using System;
+using System.Collections.Generic;
+using System.Drawing.Printing;
+
+namespace Recognizer;
 
 public static class ThresholdFilterTask
 {
 	public static double[,] ThresholdFilter(double[,] original, double whitePixelsFraction)
 	{
-		return new double[original.GetLength(0), original.GetLength(1)];
+        var n = original.Length;
+        var minNumberAboveTreshold = (int)(n * whitePixelsFraction);
+
+        var originalValues = GetSortedValues(original);
+
+        var treshold = double.MaxValue;
+
+        if (minNumberAboveTreshold != 0)
+            treshold = originalValues[n - minNumberAboveTreshold];
+
+        var xSize = original.GetLength(0);
+        var ySize = original.GetLength(1);
+        var result = new double[xSize, ySize];
+
+        for (var row = 0; row < ySize; row++)
+        {
+            for (var col = 0; col < xSize; col++)
+            {
+                if (original[col, row] >= treshold)
+                result[col, row] = 1;                
+            }
+        }
+        return result;
 	}
+
+	private static List<double> GetSortedValues(double[,] original)
+	{	
+		var result = new List<double>(original.Length);
+		foreach (var value in original)
+		{
+			result.Add(value);
+		}
+		result.Sort();
+		return result;			;
+	}	
 }
